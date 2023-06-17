9209750980ed89a739eedaf9e5d138531e26dedb
Andrea Sanchez Liporace <72173364+andreaSanchezLiporace@users.noreply.github.com>
2023-06-17 18:32:52 -0300

    Agregamos a Lucas

--- civ/front/src/pages/footer.jsx
+++ civ/front/src/pages/footer.jsx
@@ -17,6 +17,7 @@ function Footer(){
                     <li>Brandi, Lucio</li>
                     <li>Costantino, Federico</li>
                     <li>Sanchez Liporacce, Andrea</li>
+                    <li>Zanutel, Lucas</li>
                 </ul>
             </div>
         </footer>
